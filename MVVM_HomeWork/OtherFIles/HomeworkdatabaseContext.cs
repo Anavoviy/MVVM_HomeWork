a3d4fd53648ef7b95153114056bb794a3c79562d
Владимир Булыгин <vovan200512345@mail.ru>
2023-05-25 09:17:38 +1000

    -добавил данные

--- MVVM_HomeWork/OtherFIles/HomeworkdatabaseContext.cs
+++ MVVM_HomeWork/OtherFIles/HomeworkdatabaseContext.cs
@@ -8,7 +8,6 @@ public partial class HomeworkdatabaseContext : DbContext
 {
     public HomeworkdatabaseContext()
     {
-        Database.EnsureDeleted();
         Database.EnsureCreated();
     }
 
@@ -93,6 +92,49 @@ public partial class HomeworkdatabaseContext : DbContext
             new Category() { Id = 12, Title = "кухонная техника" },
             new Category() { Id = 13, Title = "аудиотехника" },
             new Category() { Id = 14, Title = "товары для дома" }
+            );
+        modelBuilder.Entity<Provider>().HasData(
+            new Provider() { Id = 1, Title = "Apple"},
+            new Provider() { Id = 2, Title = "ASICS" },
+            new Provider() { Id = 3, Title = "ASUS" },
+            new Provider() { Id = 4, Title = "Davidoff" },
+            new Provider() { Id = 5, Title = "Ferrero" },
+            new Provider() { Id = 6, Title = "IKEA" },
+            new Provider() { Id = 7, Title = "JBL" },
+            new Provider() { Id = 8, Title = "Koh-I-Noor" },
+            new Provider() { Id = 9, Title = "LG" },
+            new Provider() { Id = 10, Title = "L'Oreal" },
+            new Provider() { Id = 11, Title = "Philips" },
+            new Provider() { Id = 12, Title = "Tefal" },
+            new Provider() { Id = 13, Title = "Under Armor" },
+            new Provider() { Id = 14, Title = "Блумсбери" },
+            new Provider() { Id = 15, Title = "Dell" },
+            new Provider() { Id = 16, Title = "Lancome" },
+            new Provider() { Id = 17, Title = "Lavazza" },
+            new Provider() { Id = 18, Title = "Lenovo" },
+            new Provider() { Id = 19, Title = "Lipton" }
+            );
+        modelBuilder.Entity<Product>().HasData(
+            new Product() { Id = 1, Title = "Книга \"Гарри Поттер и филосовский камень\"", IdCategory = 1, IdProvider = 14, CostOfOne = 500, Discount = 10 },
+            new Product() { Id = 2, Title = "Смартфон iPhone 12 Pro", IdCategory = 2, IdProvider = 1, CostOfOne = 999900, Discount = 5 },
+            new Product() { Id = 3, Title = "Ноутбук IdeaPad 3", IdCategory = 2, IdProvider = 18, CostOfOne = 44990, Discount = 15 },
+            new Product() { Id = 4, Title = "Кроссовки Gel-Rayano 27", IdCategory = 3, IdProvider = 2, CostOfOne = 12990, Discount = 20 },
+            new Product() { Id = 5, Title = "Шоколадная конфета Ferrero Rocher", IdCategory = 4, IdProvider = 5, CostOfOne = 150, Discount = 0 },
+            new Product() { Id = 6, Title = "Рюкзак Back To School", IdCategory = 5, IdProvider = 13, CostOfOne = 2990, Discount = 10 },
+            new Product() { Id = 7, Title = "Кофе в зернах \"Arabica\"", IdCategory = 4, IdProvider = 17, CostOfOne = 700, Discount = 0 },
+            new Product() { Id = 8, Title = "Стиральная машина WM 1080", IdCategory = 6, IdProvider = 9, CostOfOne = 29990, Discount = 7 },
+            new Product() { Id = 9, Title = "Шампунь \"Elseve\" для волос", IdCategory = 7, IdProvider = 10, CostOfOne = 300, Discount = 8 },
+            new Product() { Id = 10, Title = "Ноутбук MAckBook air", IdCategory = 2, IdProvider = 1, CostOfOne = 89990, Discount = 5 },
+            new Product() { Id = 11, Title = "Парфюм \"La Vie Est Belle\"", IdCategory = 8, IdProvider = 16, CostOfOne = 7900, Discount = 12 },
+            new Product() { Id = 12, Title = "Керамическая сковорода \"Titanium\"", IdCategory = 9, IdProvider = 12, CostOfOne = 1590, Discount = 20 },
+            new Product() { Id = 13, Title = "Набор фломастеров \"Capitan\"", IdCategory = 10, IdProvider = 8, CostOfOne = 490, Discount = 0 },
+            new Product() { Id = 14, Title = "Чай \"Golden Tea\"", IdCategory = 4, IdProvider = 19, CostOfOne = 250, Discount = 15 },
+            new Product() { Id = 15, Title = "Монитор UltraSharp", IdCategory = 11, IdProvider = 15, CostOfOne = 33990, Discount = 10 },
+            new Product() { Id = 16, Title = "Блендер UltraBlend", IdCategory = 12, IdProvider = 11, CostOfOne = 5990, Discount = 0 },
+            new Product() { Id = 17, Title = "Маршрутизатор RT-AC86U", IdCategory = 11, IdProvider = 3, CostOfOne = 16490, Discount = 5 },
+            new Product() { Id = 18, Title = "Колонки Xtreme 3", IdCategory = 13, IdProvider = 7, CostOfOne = 14990, Discount = 10 },
+            new Product() { Id = 19, Title = "Гель для душа \"Cool Water\"", IdCategory = 8, IdProvider = 4, CostOfOne = 590, Discount = 0 },
+            new Product() { Id = 20, Title = "Лампа настольная \"Ella\"", IdCategory = 14, IdProvider = 6, CostOfOne = 1590, Discount = 15 }
             );
     }
 
